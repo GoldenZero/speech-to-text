89b32b07749b657c2463007c5a7519cf5b72a688
lenabayeva <lenabayeva@gmail.com>
2017-11-13 13:06:39 +0100

    Fix static method bug + new way to open file

--- TextToPOS.py
+++ TextToPOS.py
@@ -1,16 +1,14 @@
 import spacy
 
 
-@staticmethod
-def load_en_large():
-    return spacy.load('en_core_web_lg')
-
-
 class TextToPOS:
-    def get_filter_text(self, text_file, pos_tags, nlp):
-        text = open().read(text_file)
-        doc = nlp(text)
+    def get_filter_text(self, text_file, nlp):
+        with open(text_file, "r") as fh:
+            text = fh.read()
+            doc = nlp(text)
 
         return [[w, w.pos_] for w in doc if w.pos_ in ['VERB', 'NOUN', 'PROPN'] ]
 
-
+    @staticmethod
+    def load_en_large():
+        return spacy.load('en_core_web_lg')
