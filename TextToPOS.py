696c0e19c4bb361beee94639181f57a618d98856
ehsankddm <ehsan.khoddam@gmail.com>
2017-11-13 14:23:08 +0100

    first working version :)

--- TextToPOS.py
+++ TextToPOS.py
@@ -2,10 +2,15 @@ import spacy
 
 
 class TextToPOS:
-    def get_filter_text(self, text_file, nlp):
+    def get_filter_file(self, text_file, nlp):
         with open(text_file, "r") as fh:
             text = fh.read()
             doc = nlp(text)
+
+        return [[w.text, w.pos_] for w in doc if w.pos_ in ['VERB', 'NOUN', 'PROPN'] ]
+    
+    def get_filter_str(self, text, nlp):
+        doc = nlp(text)
 
         return [[w.text, w.pos_] for w in doc if w.pos_ in ['VERB', 'NOUN', 'PROPN'] ]
 
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from TextToPOS import TextToPOS
 from transcribe import transcribe_gcs
+import os
+
 def main():
     base_str="gs://saas-devel/test/segment"
     uris=[]
-    for i in range (1,12):
+    for i in range (1,22):
         uris.append("gs://saas-devel/test/segment{0}.flac".format(i))
     print (uris)
-    
+    #for file_names in os.listdir(./data/):
+    #    print(file_names)
+    t2p = TextToPOS()
     nlp = TextToPOS.load_en_large()
     for uri in uris:
         text = transcribe_gcs(uri)
-        result = TextToPOS.get_filter_text(text, nlp)
+        result = t2p.get_filter_str(text, nlp)
         print(uri," will have: \n",result)
        
 if __name__=='__main__':
